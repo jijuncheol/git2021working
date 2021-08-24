90251bbb6504c7e1506a2e73aee1b2956a298642
jijunjcheol <flix1242@naver.com>
2021-08-25 01:26:25 +0900

    20210825 react

--- 4-react/myworkspace/src/components/Feed.tsx
+++ 4-react/myworkspace/src/components/Feed.tsx
@@ -1,27 +1,122 @@
+import React, { useRef, useState } from "react";
+
+import produce, { Immer } from "immer";
+
+
+interface FeedState {
+  id: number;
+  content?: string | undefined;
+  dataUrl?: string | undefined;
+  fileType?: string | undefined;
+  createTime: number;
+}
+
+const getTimeString = (unixtime: number) => {
+  const dateTime = new Date(unixtime);
+  return `${dateTime.toLocaleDateString()} ${dateTime.toLocaleTimeString()}`;
+}
+
 const Feed = () => {
+  const [feedList, setFeedList] = useState<FeedState[]>([
+  ]);
+  const formRef = useRef<HTMLFormElement>(null);
+  const fileRef = useRef<HTMLInputElement>(null);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+
+
+  const add = (e: React.KeyboardEvent<HTMLInputElement> | null) => {
+    if (e) {
+      if (e.code !== "Enter") return;
+    }
+
+    if (fileRef.current?.files?.length) {
+      const file = fileRef.current?.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        post(reader.result?.toString(), file.type);
+      };
+    } else {
+      post(undefined, undefined);
+    }
+  };
+
+  const post = (dataUrl: string | undefined, fileType: string | undefined) => {
+    const feed: FeedState = {
+      id: feedList.length > 0 ? feedList[0].id + 1 : 1,
+      content: textRef.current?.value,
+      dataUrl: dataUrl,
+      fileType: fileType,
+      createTime: new Date().getTime(),
+    };
+    setFeedList(
+      produce((state) => {
+        state.unshift(feed);
+      })
+    );
+    formRef.current?.reset();
+  }
+
+
+
+  const del = (id: number) => {
+    setFeedList(feedList.filter((item) => item.id !== id));
+  }
+
   return (
     <>
+      <h2 className="text-conter my-5">Feed</h2>
       <form
         className="mt-5"
         onSubmit={(e) => {
           e.preventDefault();
         }}
+        ref={formRef}
       >
         <textarea
+          style={{ boxSizing: "border-box", height: "15vh" }}
           className="form-control mb-1 w-100"
           placeholder="Leave a post here"
+          ref={textRef}
         ></textarea>
         <div className="d-flex">
           <input
             type="file"
             className="form-control me-1"
             accept="image/png, image/jpeg, video/mp4"
+            ref={fileRef}
           />
-          <button className="btn btn-primary text-nowrap" type="button">
+          <button
+            className="btn btn-primary text-nowrap"
+            type="button"
+            onClick={() => {
+              add(null);
+            }}
+          >
             입력
           </button>
         </div>
       </form>
+      <div className="card mt-1">
+        {feedList.map(item => (
+          <div className="card-body">
+            <img src={item.dataUrl} className="card-img-top" alt="..."></img>
+            <p className="card-text">{item.content}</p>
+            <span>
+              - {getTimeString(item.createTime)}
+            </span>
+            <a href="#"
+              className="link-secondary fs-6 float-end"
+              onClick={() => {
+                del(item.id);
+              }}
+            >
+              삭제
+            </a>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
--- 4-react/myworkspace/src/components/Todo.tsx
+++ 4-react/myworkspace/src/components/Todo.tsx
@@ -1,13 +1,12 @@
 import { useRef, useState } from "react";
-import Alert from "./base/Alert";
+
+import produce from "immer";
 
 // 1건에 대한 타입
 interface TodoState {
   id: number;
   memo: string | undefined;
   createTime: number;
-  modifyTime?: number;
-  isEdit?: boolean; // 수정모드인지 여부
 }
 
 const getTimeString = (unixtime: number) => {
@@ -18,84 +17,37 @@ const getTimeString = (unixtime: number) => {
 };
 
 const Todo = () => {
-  // todo 여러건에 대한 state
-  // 참고) new Date().getTime() -> unix time 생성됨
   const [todoList, setTodoList] = useState<TodoState[]>([
     { id: 2, memo: "Typescript", createTime: new Date().getTime() },
     { id: 1, memo: "React State 연습", createTime: new Date().getTime() },
   ]);
 
-  // 빈 값 여부 state
-  const [isError, setIsError] = useState(false);
-
   const inputRef = useRef<HTMLInputElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
   const ulRef = useRef<HTMLUListElement>(null);
 
-  const add = (e: React.KeyboardEvent<HTMLInputElement> | null) => {
-    // 이벤트 객체가 있을 때는 입력박스에서 엔터 입력
-    if (e) {
-      if (e.code !== "Enter") return;
-    }
-
-    // 입력값이 없으면 에러 메시지 표시
-    if (!inputRef.current?.value) {
-      setIsError(true);
-      return;
-    }
-
+  const add = () => {
     const todo: TodoState = {
       id: todoList.length > 0 ? todoList[0].id + 1 : 1,
-      // optional chaning
       memo: inputRef.current?.value,
       createTime: new Date().getTime(),
     };
-
-    setTodoList([todo, ...todoList]);
+    setTodoList(
+      produce((state) => {
+        state.unshift(todo);
+      })
+    );
 
     // 입력값 초기화
     formRef.current?.reset();
-    // 에러 메시지 제거
-    setIsError(false);
   };
 
-  const del = (id: number) => {
-    // 불변성 때문에 splice를 사용할 수 없음
-    // 주로 filter 함수를 사용
-    // filter 함수로 해당 id를 제외하고 새로운 배열로 리턴함.
-    setTodoList(todoList.filter((item) => item.id !== id));
-  };
-
-  const edit = (id: number, mod: boolean) => {
-    // 해당 id에 해당하는 item만 edit 모드로 변경함
-    // 해당 item의 속성을 변경한 후 변경된 item을 반환
-    // map 함수는 새로운 배열을 반환하는 함수, 배열길이는 기존 배열 길이와 같음
+  const del = (id: number, index: number) => {
+    console.log(id);
+    // immer로 state 배열 직접 조작(index로 삭제)
     setTodoList(
-      todoList.map((item) => {
-        if (item.id === id) {
-          item.isEdit = mod;
-        }
-
-        return item;
-      })
-    );
-  };
-
-  const save = (id: number, index: number) => {
-    console.log(ulRef.current);
-
-    // ul 밑에 있는 입력박스중에서 index번째 입력박스만 선택
-    const input = ulRef.current?.querySelectorAll("input")[index];
-    setTodoList(
-      todoList.map((item) => {
-        // 해당 id의 item의 값을 변경
-        if (item.id === id) {
-          item.memo = input?.value;
-          item.modifyTime = new Date().getTime();
-          item.isEdit = false;
-        }
-
-        return item;
+      produce((state) => {
+        state.splice(index, 1);
       })
     );
   };
@@ -106,11 +58,6 @@ const Todo = () => {
       <form
         className="d-flex"
         ref={formRef}
-        /* 
-          event.preventDefault(); 
-          - 기본이벤트 작업을 처리하지 않음 
-          - submit form
-        */
         onSubmit={(e) => e.preventDefault()}
       >
         <input
@@ -118,95 +65,35 @@ const Todo = () => {
           className="form-control me-2"
           placeholder="할 일 ..."
           ref={inputRef}
-          onKeyPress={(e) => {
-            add(e);
-          }}
         />
         <button
           type="button"
           className="btn btn-primary text-nowrap"
           onClick={() => {
-            add(null);
+            add();
           }}
         >
           추가
         </button>
       </form>
-      {isError && (
-        <Alert
-          message={"내용을 입력해주세요."}
-          variant={"danger"}
-          // 닫기 버튼을 클릭할 때 처리하는 함수를 넘김
-          onClose={() => {
-            setIsError(false);
-          }}
-        />
-      )}
       <ul id="ul-list" className="list-group list-group-flush mt-3" ref={ulRef}>
-        {todoList.length === 0 && (
-          <li className="list-group-item">데이터가 없습니다.</li>
-        )}
         {/* 데이터와 UI요소 바인딩 */}
         {todoList.map((item, index) => (
           <li className="list-group-item d-flex" key={item.id}>
             <div className="w-100">
-              {/* 보기모드일 때 보이는 내용 */}
-              {!item.isEdit && <span className="me-1">{item.memo}</span>}
-              {!item.isEdit && (
-                <span style={{ fontSize: "0.75rem" }}>
-                  -{" "}
-                  {getTimeString(
-                    item.modifyTime ? item.modifyTime : item.createTime
-                  )}
-                </span>
-              )}
-              {/* 수정모드일 때 보이는 입력폼 */}
-              {item.isEdit && (
-                <input type="text" className="w-100" defaultValue={item.memo} />
-              )}
+              <span className="me-1">{item.memo}</span>
+              <span style={{ fontSize: "0.75rem" }}>
+                - {getTimeString(item.createTime)}
+              </span>
             </div>
-            {/* 보기모드일 때 보이는 버튼 */}
-            {!item.isEdit && (
-              <button
-                className="btn btn-outline-secondary btn-sm ms-2 me-1 text-nowrap"
-                onClick={() => {
-                  edit(item.id, true);
-                }}
-              >
-                수정
-              </button>
-            )}
-            {!item.isEdit && (
-              <button
-                className="btn btn-outline-secondary btn-sm text-nowrap"
-                onClick={() => {
-                  del(item.id);
-                }}
-              >
-                삭제
-              </button>
-            )}
-            {/* 수정모드일 때 보이는 버튼 */}
-            {item.isEdit && (
-              <button
-                className="btn btn-outline-secondary btn-sm ms-2 me-1 text-nowrap"
-                onClick={() => {
-                  save(item.id, index);
-                }}
-              >
-                저장
-              </button>
-            )}
-            {item.isEdit && (
-              <button
-                className="btn btn-outline-secondary btn-sm text-nowrap"
-                onClick={() => {
-                  edit(item.id, false);
-                }}
-              >
-                취소
-              </button>
-            )}
+            <button
+              className="btn btn-outline-secondary btn-sm text-nowrap"
+              onClick={() => {
+                del(item.id, index);
+              }}
+            >
+              삭제
+            </button>
           </li>
         ))}
       </ul>
